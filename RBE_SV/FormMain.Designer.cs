c0d9057f3c523d54a3bd90efaabf8cbd884d86de
FerRamBCS <jfer1302@gmail.com>
2024-10-14 21:31:57 -0700

    MAINFORM animacion DONE BOTONES ON PROGRESS

--- RBE_SV/FormMain.Designer.cs
+++ RBE_SV/FormMain.Designer.cs
@@ -31,13 +31,17 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Opc = new System.Windows.Forms.PictureBox();
             this.panel4 = new System.Windows.Forms.Panel();
             this.Btn_Logout = new System.Windows.Forms.Button();
             this.FLP_MConntainer = new System.Windows.Forms.FlowLayoutPanel();
             this.panel9 = new System.Windows.Forms.Panel();
             this.Btn_Consultas = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.Btn_CPrecios = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.Btn_CError = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.Btn_Ventas = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
@@ -45,44 +49,31 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.Btn_Home = new System.Windows.Forms.Button();
             this.FLP_Main = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.Btn_CPrecios = new System.Windows.Forms.Button();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.Btn_CError = new System.Windows.Forms.Button();
             this.T_Menu = new System.Windows.Forms.Timer(this.components);
+            this.T_SB = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Btn_Opc)).BeginInit();
             this.panel4.SuspendLayout();
             this.FLP_MConntainer.SuspendLayout();
             this.panel9.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel2.SuspendLayout();
             this.FLP_Main.SuspendLayout();
-            this.panel6.SuspendLayout();
-            this.panel7.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(177)))), ((int)(((byte)(177)))));
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.Btn_Opc);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1087, 43);
             this.panel1.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(10, 7);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 29);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
@@ -93,10 +84,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "SISTEMA DE VENTAS RAMIREZ BRAVO  EQUIPOS\r\n";
             // 
+            // Btn_Opc
+            // 
+            this.Btn_Opc.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Opc.Image")));
+            this.Btn_Opc.Location = new System.Drawing.Point(10, 7);
+            this.Btn_Opc.Name = "Btn_Opc";
+            this.Btn_Opc.Size = new System.Drawing.Size(37, 29);
+            this.Btn_Opc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Btn_Opc.TabIndex = 1;
+            this.Btn_Opc.TabStop = false;
+            this.Btn_Opc.Click += new System.EventHandler(this.Btn_Opc_Click);
+            // 
             // panel4
             // 
             this.panel4.Controls.Add(this.Btn_Logout);
-            this.panel4.Location = new System.Drawing.Point(420, 108);
+            this.panel4.Location = new System.Drawing.Point(3, 299);
             this.panel4.Name = "panel4";
             this.panel4.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
             this.panel4.Size = new System.Drawing.Size(330, 63);
@@ -123,9 +125,9 @@
             this.FLP_MConntainer.Controls.Add(this.panel9);
             this.FLP_MConntainer.Controls.Add(this.panel6);
             this.FLP_MConntainer.Controls.Add(this.panel7);
-            this.FLP_MConntainer.Location = new System.Drawing.Point(634, 219);
+            this.FLP_MConntainer.Location = new System.Drawing.Point(3, 230);
             this.FLP_MConntainer.Name = "FLP_MConntainer";
-            this.FLP_MConntainer.Size = new System.Drawing.Size(330, 191);
+            this.FLP_MConntainer.Size = new System.Drawing.Size(330, 63);
             this.FLP_MConntainer.TabIndex = 7;
             // 
             // panel9
@@ -153,6 +155,59 @@
             this.Btn_Consultas.Text = "           Consultas";
             this.Btn_Consultas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Btn_Consultas.UseVisualStyleBackColor = false;
+            this.Btn_Consultas.Click += new System.EventHandler(this.Btn_Consultas_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.Btn_CPrecios);
+            this.panel6.Location = new System.Drawing.Point(0, 63);
+            this.panel6.Margin = new System.Windows.Forms.Padding(0);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
+            this.panel6.Size = new System.Drawing.Size(330, 63);
+            this.panel6.TabIndex = 9;
+            // 
+            // Btn_CPrecios
+            // 
+            this.Btn_CPrecios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(147)))), ((int)(((byte)(147)))));
+            this.Btn_CPrecios.Font = new System.Drawing.Font("Bahnschrift", 9F);
+            this.Btn_CPrecios.Image = ((System.Drawing.Image)(resources.GetObject("Btn_CPrecios.Image")));
+            this.Btn_CPrecios.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_CPrecios.Location = new System.Drawing.Point(-16, -7);
+            this.Btn_CPrecios.Margin = new System.Windows.Forms.Padding(0);
+            this.Btn_CPrecios.Name = "Btn_CPrecios";
+            this.Btn_CPrecios.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.Btn_CPrecios.Size = new System.Drawing.Size(356, 85);
+            this.Btn_CPrecios.TabIndex = 0;
+            this.Btn_CPrecios.Text = "           Consultar precios";
+            this.Btn_CPrecios.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_CPrecios.UseVisualStyleBackColor = false;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.Btn_CError);
+            this.panel7.Location = new System.Drawing.Point(0, 126);
+            this.panel7.Margin = new System.Windows.Forms.Padding(0);
+            this.panel7.Name = "panel7";
+            this.panel7.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
+            this.panel7.Size = new System.Drawing.Size(330, 63);
+            this.panel7.TabIndex = 9;
+            // 
+            // Btn_CError
+            // 
+            this.Btn_CError.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(147)))), ((int)(((byte)(147)))));
+            this.Btn_CError.Font = new System.Drawing.Font("Bahnschrift", 9F);
+            this.Btn_CError.Image = ((System.Drawing.Image)(resources.GetObject("Btn_CError.Image")));
+            this.Btn_CError.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_CError.Location = new System.Drawing.Point(-16, -7);
+            this.Btn_CError.Margin = new System.Windows.Forms.Padding(0);
+            this.Btn_CError.Name = "Btn_CError";
+            this.Btn_CError.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.Btn_CError.Size = new System.Drawing.Size(356, 85);
+            this.Btn_CError.TabIndex = 0;
+            this.Btn_CError.Text = "           Consultar errores";
+            this.Btn_CError.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_CError.UseVisualStyleBackColor = false;
             // 
             // panel3
             // 
@@ -232,68 +287,25 @@
             this.FLP_Main.Controls.Add(this.panel2);
             this.FLP_Main.Controls.Add(this.panel5);
             this.FLP_Main.Controls.Add(this.panel3);
+            this.FLP_Main.Controls.Add(this.FLP_MConntainer);
+            this.FLP_Main.Controls.Add(this.panel4);
             this.FLP_Main.Dock = System.Windows.Forms.DockStyle.Left;
             this.FLP_Main.Location = new System.Drawing.Point(0, 43);
             this.FLP_Main.Name = "FLP_Main";
             this.FLP_Main.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
-            this.FLP_Main.Size = new System.Drawing.Size(329, 577);
+            this.FLP_Main.Size = new System.Drawing.Size(200, 577);
             this.FLP_Main.TabIndex = 1;
-            // 
-            // panel6
-            // 
-            this.panel6.Controls.Add(this.Btn_CPrecios);
-            this.panel6.Location = new System.Drawing.Point(0, 63);
-            this.panel6.Margin = new System.Windows.Forms.Padding(0);
-            this.panel6.Name = "panel6";
-            this.panel6.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
-            this.panel6.Size = new System.Drawing.Size(330, 63);
-            this.panel6.TabIndex = 9;
-            // 
-            // Btn_CPrecios
-            // 
-            this.Btn_CPrecios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(177)))), ((int)(((byte)(177)))));
-            this.Btn_CPrecios.Font = new System.Drawing.Font("Bahnschrift", 9F);
-            this.Btn_CPrecios.Image = ((System.Drawing.Image)(resources.GetObject("Btn_CPrecios.Image")));
-            this.Btn_CPrecios.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_CPrecios.Location = new System.Drawing.Point(-16, -7);
-            this.Btn_CPrecios.Margin = new System.Windows.Forms.Padding(0);
-            this.Btn_CPrecios.Name = "Btn_CPrecios";
-            this.Btn_CPrecios.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
-            this.Btn_CPrecios.Size = new System.Drawing.Size(356, 85);
-            this.Btn_CPrecios.TabIndex = 0;
-            this.Btn_CPrecios.Text = "           Consultar precios";
-            this.Btn_CPrecios.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_CPrecios.UseVisualStyleBackColor = false;
-            // 
-            // panel7
-            // 
-            this.panel7.Controls.Add(this.Btn_CError);
-            this.panel7.Location = new System.Drawing.Point(0, 126);
-            this.panel7.Margin = new System.Windows.Forms.Padding(0);
-            this.panel7.Name = "panel7";
-            this.panel7.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
-            this.panel7.Size = new System.Drawing.Size(330, 63);
-            this.panel7.TabIndex = 9;
-            // 
-            // Btn_CError
-            // 
-            this.Btn_CError.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(177)))), ((int)(((byte)(177)))));
-            this.Btn_CError.Font = new System.Drawing.Font("Bahnschrift", 9F);
-            this.Btn_CError.Image = ((System.Drawing.Image)(resources.GetObject("Btn_CError.Image")));
-            this.Btn_CError.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_CError.Location = new System.Drawing.Point(-16, -7);
-            this.Btn_CError.Margin = new System.Windows.Forms.Padding(0);
-            this.Btn_CError.Name = "Btn_CError";
-            this.Btn_CError.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
-            this.Btn_CError.Size = new System.Drawing.Size(356, 85);
-            this.Btn_CError.TabIndex = 0;
-            this.Btn_CError.Text = "           Consultar errores";
-            this.Btn_CError.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_CError.UseVisualStyleBackColor = false;
+            this.FLP_Main.Paint += new System.Windows.Forms.PaintEventHandler(this.FLP_Main_Paint);
             // 
             // T_Menu
             // 
+            this.T_Menu.Interval = 10;
             this.T_Menu.Tick += new System.EventHandler(this.T_Menu_Tick);
+            // 
+            // T_SB
+            // 
+            this.T_SB.Interval = 10;
+            this.T_SB.Tick += new System.EventHandler(this.T_SB_Tick);
             // 
             // FormMain
             // 
@@ -302,8 +314,6 @@
             this.ClientSize = new System.Drawing.Size(1087, 620);
             this.Controls.Add(this.FLP_Main);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.FLP_MConntainer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -313,16 +323,16 @@
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Btn_Opc)).EndInit();
             this.panel4.ResumeLayout(false);
             this.FLP_MConntainer.ResumeLayout(false);
             this.panel9.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.FLP_Main.ResumeLayout(false);
-            this.panel6.ResumeLayout(false);
-            this.panel7.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -330,7 +340,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Btn_Opc;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Button Btn_Logout;
@@ -349,5 +359,6 @@
         private System.Windows.Forms.Button Btn_Home;
         private System.Windows.Forms.FlowLayoutPanel FLP_Main;
         private System.Windows.Forms.Timer T_Menu;
+        private System.Windows.Forms.Timer T_SB;
     }
 }
--- RBE_SV/FormMain.cs
+++ RBE_SV/FormMain.cs
@@ -22,7 +22,78 @@ namespace RBE_SV
 
         }
 
+
+        bool MenuExpand = false;
         private void T_Menu_Tick(object sender, EventArgs e)
+        {
+            // Definir las alturas máxima y mínima
+            int maxHeight = 170; // Altura máxima deseada
+            int minHeight = 50;  // Altura mínima deseada
+            int increment = 10;  // Incremento en la altura
+
+            if (MenuExpand == false)
+            {
+                // Expandir el menú
+                FLP_MConntainer.Height += increment;
+                if (FLP_MConntainer.Height >= maxHeight)
+                {
+                    FLP_MConntainer.Height = maxHeight; // Asegúrate de que no exceda la altura máxima
+                    T_Menu.Stop();
+                    MenuExpand = true;
+                }
+            }
+            else
+            {
+                // Contraer el menú
+                FLP_MConntainer.Height -= increment;
+                if (FLP_MConntainer.Height <= minHeight)
+                {
+                    FLP_MConntainer.Height = minHeight; // Asegúrate de que no baje de la altura mínima
+                    T_Menu.Stop();
+                    MenuExpand = false;
+                }
+            }
+
+        }
+
+        private void Btn_Consultas_Click(object sender, EventArgs e)
+        {
+            T_Menu.Start();// Inicia el temporizador al hacer clic
+        }
+
+        bool sideBarExpand = true;
+
+        private void T_SB_Tick(object sender, EventArgs e)
+        {
+            // Si la barra lateral está expandida
+            if (sideBarExpand)
+            {
+                FLP_Main.Width -= 10; // Contraer la barra
+                if (FLP_Main.Width <= 50)
+                {
+                    FLP_Main.Width = 50;
+                    sideBarExpand = false; // Cambia el estado a contraído
+                    T_SB.Stop(); // Detén el temporizador
+                }
+            }
+            else // Si la barra lateral está contraída
+            {
+                FLP_Main.Width += 10; // Expandir la barra
+                if (FLP_Main.Width >= 200)
+                {
+                    FLP_Main.Width = 200;
+                    sideBarExpand = true; // Cambia el estado a expandido
+                    T_SB.Stop(); // Detén el temporizador
+                }
+            }
+        }
+
+        private void Btn_Opc_Click(object sender, EventArgs e)
+        {
+            T_SB.Start();// Inicia el temporizador al hacer clic
+        }
+
+        private void FLP_Main_Paint(object sender, PaintEventArgs e)
         {
 
         }
